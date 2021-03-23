e36ff1b497a81c6577371c685f6ae45d8c492e31
Léopold Guérin <leopold.guerin@gmail.com>
2021-03-23 20:19:19 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,149 @@
+import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Table from 'react-bootstrap/Table';
+import VotingContract from "./contracts/Voting.json";
+import getWeb3 from "./getWeb3";
+
+import "./App.css";
+
+class App extends Component {
+  state = { web3: null, accounts: null, contract: null, voters: null };
+
+  enumStatus = [
+    { name: "Registering Voters", color: "green" },
+    { name: "Proposals Registration Started", color: "green" },
+    { name: "Proposals Registration Ended", color: "green" },
+    { name: "Voting Session Started", color: "green" },
+    { name: "Voting Session Ended", color: "green" },
+    { name: "Votes Tallied", color: "green" }
+  ];
+
+  componentDidMount = async () => {
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = VotingContract.networks[networkId];
+      const instance = new web3.eth.Contract(
+        VotingContract.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      this.setState({ web3, accounts, contract: instance }, this.runInit);
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
+    }
+  };
+
+  runInit = async () => {
+    const { contract } = this.state;
+
+    // récupérer la liste des comptes autorisés
+    const voters = await contract.methods.getAddresses().call();
+    const status = await contract.methods.status().call()
+    const statusName = this.enumStatus[status].name
+    const statusColor = this.enumStatus[status].color
+
+    // Mettre à jour le state 
+    this.setState({ voters: voters, statusName: statusName, statusColor: statusColor });
+  }
+
+  registerVoter = async () => {
+    const { accounts, contract } = this.state;
+    const address = this.address.value;
+
+    // Interaction avec le smart contract pour ajouter un compte 
+    await contract.methods.registerVoter(address).send({from: accounts[0]});
+    // Récupérer la liste des comptes autorisés
+    this.runInit();
+  }
+
+  startProposalregistration = async() => {
+    const { accounts, contract } = this.state;
+    await contract.methods.startProposalregistration().send({from: accounts[0]});
+
+    this.runInit();
+  }
+
+  render() {
+    const { voters, statusColor, statusName } = this.state;
+    if (!this.state.web3) {
+      return <div>Loading Web3, accounts, and contract...</div>;
+    }
+    return (
+      <div className="App">
+        <span style={{display: 'flex', justifyContent: 'center', padding: "5px", color: "white", border: "1px solid green", backgroundColor: statusColor}}>
+          { statusName }
+        </span>
+        <div>
+            <h2 className="text-center">Système de vote</h2>
+            <hr></hr>
+            <br></br>
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <Card style={{ width: '50rem' }}>
+            <Card.Header><strong>Liste des votants</strong></Card.Header>
+            <Card.Body>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Table striped bordered hover>
+                    <thead>
+                      <tr>
+                        <th>@</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {voters !== null &&
+                        voters.map((a) => <tr><td>{a}</td></tr>)
+                      }
+                    </tbody>
+                  </Table>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </div>
+        <br></br>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <Card style={{ width: '50rem' }}>
+            <Card.Header><strong>Autoriser un nouveau compte</strong></Card.Header>
+            <Card.Body>
+              <Form.Group controlId="formAddress">
+                <Form.Control type="text" id="address"
+                ref={(input) => { this.address = input }}
+                />
+              </Form.Group>
+              <Button onClick={ this.registerVoter } variant="dark" > Autoriser </Button>
+            </Card.Body>
+          </Card>
+          </div>
+          <div style={{display: 'flex', justifyContent: 'center'}}>
+            <Card style={{ width: '50rem' }}>
+              <Card.Header><strong>Commencer la session d'enregistrement de la proposition.</strong></Card.Header>
+              <Card.Body>
+                <Button onClick={ this.startProposalregistration } variant="dark" > Démarrer </Button>
+              </Card.Body>
+            </Card>
+          </div>
+        <br></br>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ contracts/Voting.sol
@@ -0,0 +1,159 @@
+// contracts/GameItem.sol
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Voting is Ownable {
+    enum WorkflowStatus {
+        RegisteringVoters,
+        ProposalsRegistrationStarted,
+        ProposalsRegistrationEnded,
+        VotingSessionStarted,
+        VotingSessionEnded,
+        VotesTallied
+    }
+    struct Voter {
+        bool isRegistered;
+        bool hasVoted;
+        uint votedProposalId;
+    }
+    struct Proposal {
+        string description;
+        uint voteCount;
+    }
+    mapping (uint => Proposal) public _proposals;
+    mapping (address => Voter) public _voters;
+    address[] public addresses; // TODO remove this (only DEBUG)
+    uint[] _proposalIds;
+    uint public winningProposalId;
+    uint private nonce = 0;
+    WorkflowStatus public status = WorkflowStatus.RegisteringVoters;
+
+    event VoterRegistered(address voterAddress);
+    event ProposalsRegistrationStarted();
+    event ProposalsRegistrationEnded();
+    event ProposalRegistered(uint proposalId);
+    event VotingSessionStarted();
+    event VotingSessionEnded();
+    event Voted (address voter, uint proposalId);
+    event VotesTallied();
+    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
+
+    function random() internal returns (uint) {
+        nonce++;
+        return uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, nonce))) % 100;
+    }
+    
+    function getProposalIdFromDescription(string memory _description) external view returns (uint) {
+        uint id = 0;
+        for(uint i=0; i<_proposalIds.length; i++) {
+            if (keccak256(abi.encodePacked((_proposals[_proposalIds[i]].description))) == keccak256(abi.encodePacked((_description)))) {
+                id = _proposalIds[i];
+                break;
+            }
+        }
+        return id;
+    }
+    
+    // TODO remove this (only DEBUG)
+    function getAddresses() public view returns(address[] memory){
+        return addresses;
+    }
+
+    function tally() external onlyOwner {
+        require(status == WorkflowStatus.VotingSessionEnded, "Wait, this is not the time to tally !");
+        require(status != WorkflowStatus.VotesTallied, "Vote already tallied.");
+        
+        uint max = 0;
+        for(uint i=0; i<_proposalIds.length; i++) {
+            if (_proposals[_proposalIds[i]].voteCount > max) {
+                max = _proposals[i].voteCount;
+                winningProposalId = _proposalIds[i];
+            }
+        }
+
+        WorkflowStatus previousStatus = status;
+        status = WorkflowStatus.VotesTallied;
+        emit WorkflowStatusChange(previousStatus, status);
+        emit VotesTallied();   
+    }
+    
+    function vote(string memory _description) external {
+        require(status == WorkflowStatus.VotingSessionStarted, "This is not yet the time to vote !");
+        require(_voters[msg.sender].isRegistered, "You must be registered to vote !");
+        require(!_voters[msg.sender].hasVoted, "You thought you could vote twice ? Nice try ...");
+
+        uint propId = this.getProposalIdFromDescription(_description);
+        require(propId > 0, "Proposal Not Found !");
+
+        _proposals[propId].voteCount++;  
+        _voters[msg.sender].hasVoted = true;
+        _voters[msg.sender].votedProposalId = propId;
+        
+        emit Voted(msg.sender, propId);
+    }
+    
+    function registerVoter(address _address) external onlyOwner {
+        require(status == WorkflowStatus.RegisteringVoters, "This is not the time for voter registration !");
+        require(!_voters[_address].isRegistered, "This voter is already registered !");
+        Voter memory voter;
+        voter.isRegistered = true;
+        voter.hasVoted = false;
+        _voters[_address] = voter;
+        addresses.push(_address); // TODO remove this (only DEBUG)
+        emit VoterRegistered(_address);
+    }
+    
+    function registerProposal(string memory _description) external {
+        require(status == WorkflowStatus.ProposalsRegistrationStarted, "This is not the time for proposal registration !");
+        require(_voters[msg.sender].isRegistered, "You must be registered to send a proposal.");
+
+        uint alreadyExistId = this.getProposalIdFromDescription(_description);
+        require(alreadyExistId == 0, "Proposal already exist !");
+
+        Proposal memory proposal;
+        proposal.description = _description;
+        proposal.voteCount = 0;
+        
+        uint proposalId = random();
+        _proposals[proposalId] = proposal;
+        _proposalIds.push(proposalId);
+        
+        emit ProposalRegistered(proposalId);
+    }
+    
+    function startProposalregistration() external onlyOwner {
+        require(status != WorkflowStatus.ProposalsRegistrationStarted, "Registration already started");
+        require(status == WorkflowStatus.RegisteringVoters, "You can only register proposals after voter registration");
+        WorkflowStatus previousStatus = status;
+        status = WorkflowStatus.ProposalsRegistrationStarted;
+        emit WorkflowStatusChange(previousStatus, status);
+        emit ProposalsRegistrationStarted();
+    }
+        
+    function endProposalregistration() external onlyOwner {
+        require(status == WorkflowStatus.ProposalsRegistrationStarted, "Registration is not started !");
+        WorkflowStatus previousStatus = status;
+        status = WorkflowStatus.ProposalsRegistrationEnded;
+        emit WorkflowStatusChange(previousStatus, status);
+        emit ProposalsRegistrationEnded();
+    }
+    
+    function startVotingSession() external onlyOwner {
+        require(status != WorkflowStatus.VotingSessionStarted, "Voting session already started");
+        require(status == WorkflowStatus.ProposalsRegistrationEnded, "You can only vote after proposal registration ended");
+        WorkflowStatus previousStatus = status;
+        status = WorkflowStatus.VotingSessionStarted;
+        emit WorkflowStatusChange(previousStatus, status);
+        emit VotingSessionStarted();
+    }
+        
+    function endVotingSession() external onlyOwner {
+        require(status == WorkflowStatus.VotingSessionStarted, "Voting session is not started !");
+        WorkflowStatus previousStatus = status;
+        status = WorkflowStatus.VotingSessionEnded;
+        emit WorkflowStatusChange(previousStatus, status);
+        emit VotingSessionEnded();
+    }
+
+}
